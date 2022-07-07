7da2c40306afe4b9972bc012780393ea031c467f
Delwar <delwar54@users.noreply.github.com>
2022-07-07 10:43:44 +0600

    Patient

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,5 @@ Route::middleware(['auth:sanctum', config('jetstream.auth_session'),'verified'])
         ->group(function () {
             Route::get('dashboard', 'App\Http\Controllers\DashboardController@index')->name('dashboard');
             Route::get('employees', 'App\Http\Controllers\EmployeesController@index')->name('employees');
+            Route::get('patients', 'App\Http\Controllers\PatientsController@index')->name('patients');
 });
