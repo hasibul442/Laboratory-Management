6e2c1e7a828ebe634d6b65284898cf35f9041f11
Hasibul Hasan <hasibulhasan442@gmail.com>
2022-07-13 10:13:22 +0600

    Top Bar

--- resources/views/Include/topbar.blade.php
+++ resources/views/Include/topbar.blade.php
@@ -8,7 +8,12 @@
 
         <li class="dropdown notification-list">
             <a class="nav-link dropdown-toggle nav-user mr-0 waves-effect waves-light" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
-                <img src="{{ asset('assets/images/users/avatar-4.jpg') }}" alt="user-image" class="rounded-circle">
+                @if(Auth::user()->id == App\Models\Employees::where(Auth::user()->id,'=', 'user_id')->get())
+                    <img src="{{ asset('assets/HMS/employee/avatar-4.jpg') }}" alt="user-image" class="rounded-circle">
+                @else
+                    <img src="{{ asset('assets/images/users/avatar-4.jpg') }}" alt="user-image" class="rounded-circle">
+                @endif
+
                 <span class="pro-user-name ml-1">
                     {{ Auth::user()->name }} <i class="mdi mdi-chevron-down"></i>
                 </span>
