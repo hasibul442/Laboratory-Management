1f7377bdf805631d06fa23c6c56a1f6704874c7a
Hasibul Hasan <hasibulhasan442@gmail.com>
2025-02-11 23:58:11 +0600

    Remove All file

--- resources/views/Include/footer.blade.php
+++
@@ -1,9 +0,0 @@
-<footer class="footer">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-12 text-center">
-                <script>document.write(new Date().getFullYear())</script> &copy; Developed By <a href="">Hasibul Hasan</a>
-            </div>
-        </div>
-    </div>
-</footer>
